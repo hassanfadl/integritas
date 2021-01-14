737272c1ae25e83394ecc121dbf599af7a37c71f
edgarMolina94 <edgar.molina@integritas.mx>
2021-01-14 11:42:44 -0600

    Date ch

--- mx_integritas_proyectos/models/task.py
+++ mx_integritas_proyectos/models/task.py
@@ -6,11 +6,11 @@ class Task(models.Model):
     _inherit = ['project.task']
 
     date_assign = fields.Datetime(string='Assigning Date', index=True, copy=False, readonly=False)
-    date_now_gmt = fields.Datetime(string='Date Now', index=True, copy=False, readonly=False ,compute='_set_date_gtm')
+    date_now_gmt = fields.Char(string='Date Now', index=True, copy=False, readonly=False ,compute='_set_date_gtm')
 
     @api.multi
     def _set_date_gtm(self):
     	for stock in self:
-    		stock.date_now_gmt = datetime.datetime.now()
+    		stock.date_now_gmt = datetime.datetime.now().strftime("%m %d %Y %H:%M:%S GMT-00:00 %Y")
         
 
