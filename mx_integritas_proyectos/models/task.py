8bc2d22566097d07fa20920a89fac8b473d751ad
edgarMolina94 <edgar.molina@integritas.mx>
2021-01-14 12:20:28 -0600

    Date final

--- mx_integritas_proyectos/models/task.py
+++ mx_integritas_proyectos/models/task.py
@@ -1,16 +1,17 @@
 from odoo import _, api, fields, models, tools
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Task(models.Model):
     _name = 'project.task'
     _inherit = ['project.task']
 
     date_assign = fields.Datetime(string='Assigning Date', index=True, copy=False, readonly=False)
-    date_now_gmt = fields.Char(string='Date Now', index=True, copy=False, readonly=False ,compute='_set_date_gtm')
+    date_now_gmt = fields.Datetime(string='Date Now', index=True, copy=False, readonly=False ,compute='_set_date_gtm')
 
     @api.multi
     def _set_date_gtm(self):
-    	for stock in self:
-    		stock.date_now_gmt = datetime.now().strftime("%m %d %Y %H:%M:%S GMT-00:00 %Y")
+    	for dato in self:
+    		date_gtm =  datetime.now() - timedelta(hours=6)
+    		dato.date_now_gmt = date_gtm
         
 
