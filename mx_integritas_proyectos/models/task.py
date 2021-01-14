105e5de8a6c0a29b628ed7b37fa2e2c28c00ef89
edgarMolina94 <edgar.molina@integritas.mx>
2021-01-14 11:16:31 -0600

    Demo campo

--- mx_integritas_proyectos/models/task.py
+++ mx_integritas_proyectos/models/task.py
@@ -1,8 +1,15 @@
-from odoo import _, api, fields, models, tools
+from odoo import _, api, fields, models, tools, pytz, datetime
+
 
 class Task(models.Model):
     _name = 'project.task'
     _inherit = ['project.task']
 
     date_assign = fields.Datetime(string='Assigning Date', index=True, copy=False, readonly=False)
+    date_now_gmt = fields.Datetime(string='Date Now', index=True, copy=False, readonly=False, ,compute='_set_date_gtm')
+
+    @api.multi
+    def _set_date_gtm(self):
+    	self.date_now_gmt = datetime.datetime.now()
+        
 
