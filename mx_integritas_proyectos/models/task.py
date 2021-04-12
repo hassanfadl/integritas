6d7fa0871990eee35f1a5ffe8e5b54edfd3a6cde
ColNacional <distribucion@colnal.mx>
2021-04-12 14:54:31 -0500

    Lista Negra

--- mx_integritas_proyectos/models/task.py
+++ mx_integritas_proyectos/models/task.py
@@ -8,7 +8,6 @@ class Task(models.Model):
     date_assign = fields.Datetime(string='Assigning Date', index=True, copy=False, readonly=False)
     date_now_gmt = fields.Datetime(string='Date Now', index=True, copy=False, readonly=False ,compute='_set_date_gtm')
 
-    @api.multi
     def _set_date_gtm(self):
     	for dato in self:
     		date_gtm =  datetime.now() - timedelta(hours=6)
