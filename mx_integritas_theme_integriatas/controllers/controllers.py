5ea2a0d13d312869e046bfe240bdcc87d97d78c2
edgarIntegritas19 <edgar.molina@integritas.mx>
2021-06-01 17:23:26 -0500

    v14 theme

--- mx_integritas_theme_integriatas/controllers/controllers.py
+++ mx_integritas_theme_integriatas/controllers/controllers.py
@@ -27,27 +27,10 @@ class MxIntegritasThemeIntegriatas(http.Controller):
 		if not bp:
 			bp=request.env['res.partner'].sudo().create({'name': name,'email':email,'phone':phone})
 
-		eti=request.env['crm.lead.tag'].sudo().search([('name', '=','Boton whatsapp')])
+		eti=request.env['crm.tag'].sudo().search([('name', '=','Boton whatsapp')])
 		if not eti:
-			eti=request.env['crm.lead.tag'].sudo().create({'name':'Boton whatsapp'})
-		tags=request.env['crm.lead.tag'].sudo().browse(eti.id)
-		oportunidad=request.env['crm.lead'].sudo().create({'name':'Oportunidad WhatsApp Sitio Web '+name,'partner_id':bp.id,'tag_ids':tags,'planned_revenue': float(0) })
+			eti=request.env['crm.tag'].sudo().create({'name':'Boton whatsapp'})
+		tags=request.env['crm.tag'].sudo().browse(eti.id)
+		oportunidad=request.env['crm.lead'].sudo().create({'name':'Oportunidad WhatsApp Sitio Web '+name,'partner_id':bp.id,'tag_ids':tags })
 
 		return str(oportunidad)
-
-#     @http.route('/mx_integritas_theme_integriatas//mx_integritas_theme_integriatas//', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/mx_integritas_theme_integriatas//mx_integritas_theme_integriatas//objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('mx_integritas_theme_integriatas/.listing', {
-#             'root': '/mx_integritas_theme_integriatas//mx_integritas_theme_integriatas/',
-#             'objects': http.request.env['mx_integritas_theme_integriatas/.mx_integritas_theme_integriatas/'].search([]),
-#         })
-
-#     @http.route('/mx_integritas_theme_integriatas//mx_integritas_theme_integriatas//objects/<model("mx_integritas_theme_integriatas/.mx_integritas_theme_integriatas/"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('mx_integritas_theme_integriatas/.object', {
-#             'object': obj
-#         })
