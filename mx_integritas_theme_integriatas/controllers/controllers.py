3e77a7823807cb6f8feeae146b8b601cfedeb836
edgarIntegritas19 <edgar.molina@integritas.mx>
2021-06-17 10:36:38 -0500

    Test demo

--- mx_integritas_theme_integriatas/controllers/controllers.py
+++ mx_integritas_theme_integriatas/controllers/controllers.py
@@ -14,11 +14,7 @@ class MxIntegritasThemeIntegriatas(http.Controller):
 		name = post.get('name')
 		email = post.get('email')
 		phone = post.get('phone')
-        
-        
-        
-        
-        
+
 		bp=request.env['res.partner'].sudo().search([('email', '=', email)])
 
 		if len(bp)>1:
@@ -27,10 +23,11 @@ class MxIntegritasThemeIntegriatas(http.Controller):
 		if not bp:
 			bp=request.env['res.partner'].sudo().create({'name': name,'email':email,'phone':phone})
 
-		eti=request.env['crm.tag'].sudo().search([('name', '=','Boton whatsapp')])
-		if not eti:
-			eti=request.env['crm.tag'].sudo().create({'name':'Boton whatsapp'})
-		tags=request.env['crm.tag'].sudo().browse(eti.id)
-		oportunidad=request.env['crm.lead'].sudo().create({'name':'Oportunidad WhatsApp Sitio Web '+name,'partner_id':bp.id,'tag_ids':tags })
+		#eti=request.env['crm.tag'].sudo().search([('name', '=','Boton whatsapp')])
+		source_id = request.env['utm.source'].sudo().search([('name', '=','Boton whatsapp')])
+		if not source_id:
+			source_id=request.env['utm.source'].sudo().create({'name':'Boton Whatsapp'})
+			#source_id = request.env['utm.source'].sudo().browse(source_id.id)
+		oportunidad = request.env['crm.lead'].sudo().create({'name':'Oportunidad WhatsApp Sitio Web '+name,'partner_id':bp.id,'source_id':[(4,source_id.id)] })
 
 		return str(oportunidad)
