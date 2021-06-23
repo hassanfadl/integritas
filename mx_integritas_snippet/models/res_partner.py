1274da806ca88f5eea9f1af64f2c38ac926be959
edgarIntegritas19 <edgar.molina@integritas.mx>
2021-06-23 17:44:27 -0500

    Correct

--- mx_integritas_snippet/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-class ResCompany(models.Model):
-    _inherit = 'res.partner'
-    client_vip = fields.Boolean(string='Cliente Website', help="Selecciona par que el cliente aparezca en la página web")
-    
