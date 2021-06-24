ce32110bba9be3047fb34ccf50143a8db9413d97
edgarIntegritas19 <edgar.molina@integritas.mx>
2021-06-24 12:17:40 -0500

    Snippet clientes

--- mx_integritas_snippet/__manifest__.py
+++ mx_integritas_snippet/__manifest__.py
@@ -27,6 +27,9 @@
         # 'security/ir.model.access.csv',
         'views/formulario.xml',
         'views/snippets.xml',
+        'views/carousel.xml',
+        'views/res_partner.xml',
+        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
