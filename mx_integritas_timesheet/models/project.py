61bc6bcea9e563207b0335de3f71fb76a63169ea
edgarIntegritas19 <edgar.molina@integritas.mx>
2021-08-03 14:43:52 -0500

    Field

--- mx_integritas_timesheet/models/project.py
+++ mx_integritas_timesheet/models/project.py
@@ -6,5 +6,13 @@ from odoo import models, fields, api
 class mx_integritas_timesheet(models.Model):
 	_inherit = 'project.task'
 
+	@api.depends('remaining_hours')
+	def compute_function(self):
+		if self.remaining_hours &lt; 0:
+			return True
+		else:
+			return False
+    	
 	is_email_timesheet_send = fields.Boolean(string='Correo Parte de Horas')
+	status_hours_exceded = fields.Boolean(compute=compute_function, string='Exceded hours')
 
