645e1d2d63b2116af95cc30a8a3ecde1d6bbc161
edgarIntegritas19 <edgar.molina@integritas.mx>
2021-08-12 17:49:54 -0500

    Test

--- l10n_mx_edi_payslip/models/hr_payslip.py
+++ l10n_mx_edi_payslip/models/hr_payslip.py
@@ -817,7 +817,7 @@ class HrPayslip(models.Model):
         #_logger.warning(objectify.fromstring(cfdi).Emisor.get('Rfc', objectify.fromstring(cfdi).Emisor.get('rfc')))
         _logger.warning("=========================")
 
-        return objectify.fromstring(cfdi)
+        return objectify.parse(cfdi)
 
     @staticmethod
     def _l10n_mx_get_serie_and_folio(number):
