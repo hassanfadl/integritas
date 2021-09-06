743ed2f5efb11179720354565f098c50f1b51c0a
edgarIntegritas19 <edgar.molina@integritas.mx>
2021-09-06 13:56:13 -0500

    Test

--- mx_integritas_theme_integriatas/controllers/controllers.py
+++ mx_integritas_theme_integriatas/controllers/controllers.py
@@ -46,7 +46,7 @@ class MxIntegritasThemeIntegriatas(http.Controller):
 		return texto
 	@http.route(['/whatsapp/request'], type='json', auth='public', methods=['POST'], website=True, csrf=False)
 	def getDataAPI(self, **post):
-		headers = request.httprequest
+		headers = request.httprequest.text
 		_logger.warning(headers)
 		_logger.warning("==========")
 		_logger.warning(str(request))
